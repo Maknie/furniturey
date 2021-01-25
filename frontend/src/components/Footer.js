1256298c5c58ebe9d2f574714d013b35e47b262c
maknie <maratov.maknie@gmail.com>
2021-01-25 19:12:16 +0600

    footer email

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Contact us for any questions on our <a href={"mailto:" + "maratov.maknie@gmail.com"}>email</a></Col>
+          <Col className='text-center py-3'>Contact us for any questions on our <a href={"mailto:" + "lajlimadina@gmail.com"}>email</a></Col>
         </Row>
         <Row>
           <Col className='text-center py-1'>Copyright &copy; Furniturey</Col>
